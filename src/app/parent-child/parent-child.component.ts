6938f630286be30e833c4611c09fdf7ec392198b
brthema072 <brunomarthe@hotmail.com>
2019-12-31 15:21:06 -0300

    utilizando annotation ViewChild para referenciar components e instancias de components

--- src/app/parent-child/parent-child.component.ts
+++ src/app/parent-child/parent-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-parent-child',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentChildComponent implements OnInit {
 
+  // @ViewChild("stopWatch2", {static:false})
+  @ViewChild(TimerComponent, {static:false})
+  private myTimer: TimerComponent;
+
+  @ViewChild("myP", {static: false})
+  private myp: ElementRef
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  start(){
+    this.myTimer.start();
+  }
+
+  stop(){
+    this.myTimer.stop();
+
+  }
+
+  clear(){
+    this.myTimer.clear();
+
+  }
+
+  ngAfterViewInit(){
+    console.log(this.myp)
+  }
 }
